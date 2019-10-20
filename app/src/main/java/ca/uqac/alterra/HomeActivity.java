33c8b5a50eaf9dfbc154f1a87850c48e75ae0d4f
Tanguy S <tanguy@sauton.xyz>
2019-10-20 17:27:52 -0400

    add camera intent when camera button is pressed

--- app/src/main/java/ca/uqac/alterra/HomeActivity.java
+++ app/src/main/java/ca/uqac/alterra/HomeActivity.java
@@ -2,15 +2,19 @@ package ca.uqac.alterra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     private MapsHandler mMapsHandler;
     private BottomSheetHandler mBottomSheetHandler;
+    private FloatingActionButton mCameraButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onStart();
         mMapsHandler = new MapsHandler(this);
         mBottomSheetHandler = new BottomSheetHandler(this);
+        mCameraButton = findViewById(R.id.cameraButton);
+        mCameraButton.setOnClickListener((view) -> dispatchTakePictureIntent());
 
         //Monitoring the bottom panel movements
         BottomSheetBehavior bottomPanelBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomPanel));
@@ -33,4 +39,17 @@ public class HomeActivity extends AppCompatActivity {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(mMapsHandler);
     }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
 }
