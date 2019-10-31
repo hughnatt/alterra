ed6a4f09ddede3f39b3530a5c12d826f8612581f
Hugh Natt <hughnatt@protonmail.com>
2019-10-31 16:34:35 -0400

    transform PicturesActivity into the profile fragment

--- app/src/main/java/ca/uqac/alterra/home/HomeProfileFragment.java
+++ app/src/main/java/ca/uqac/alterra/home/HomeProfileFragment.java
@@ -1,18 +1,40 @@
 package ca.uqac.alterra.home;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
 
 import ca.uqac.alterra.R;
 
 public class HomeProfileFragment extends Fragment {
+
+    FirebaseAuth mAuth;
+    FirebaseUser mCurrentUser;
+    FirebaseFirestore mFirestore;
+    FirebaseStorage mStorage;
+
+    RecyclerView mRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,11 +45,52 @@ public class HomeProfileFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        DrawerLayout navDrawer =getActivity().findViewById(R.id.navDrawer);
+        DrawerLayout navDrawer = getActivity().findViewById(R.id.navDrawer);
         Toolbar toolbar = getView().findViewById(R.id.toolbar);
         ((HomeActivity) getActivity()).setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(),navDrawer,toolbar,R.string.app_name,R.string.app_name);
         navDrawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        mFirestore = FirebaseFirestore.getInstance();
+        mStorage = FirebaseStorage.getInstance();
+
+        mRecyclerView = getView().findViewById(R.id.recyclerview);
+        GridLayoutManager mGridLayoutManager = new GridLayoutManager(getContext(), 2);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        PicturesAdapter myAdapter = new PicturesAdapter(getContext());
+        mRecyclerView.setAdapter(myAdapter);
+
+        mFirestore.collection("photos")
+                .whereEqualTo("owner", mCurrentUser.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String link = (String) document.getData().get("link");
+                                mStorage.getReference().child(link).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        System.out.println(uri.toString());
+                                        myAdapter.addPicture(uri.toString());
+                                        myAdapter.notifyItemInserted(myAdapter.getItemCount());
+
+                                    }
+                                });
+
+                            }
+
+
+                        }
+                    }
+                });
+
+
     }
 }
