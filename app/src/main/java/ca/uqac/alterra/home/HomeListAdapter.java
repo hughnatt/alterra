eca0b930cc81eb599b335d3445b3c88b63c812e7
mathuld <mathieu.vincent1997@orange.fr>
2019-11-07 20:57:59 -0500

    Create list display

---
+++ app/src/main/java/ca/uqac/alterra/home/HomeListAdapter.java
@@ -0,0 +1,65 @@
+package ca.uqac.alterra.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ca.uqac.alterra.R;
+
+public class HomeListAdapter extends RecyclerView.Adapter {
+
+    private Context mContext;
+    private ArrayList<HomeListDataModel> mDataList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imgView;
+        public TextView textView;
+        public LinearLayout linearLayout;
+
+        public MyViewHolder(View v) {
+            super(v);
+            imgView = v.findViewById(R.id.recyclerviewImage);
+            textView = v.findViewById(R.id.recyclerviewText);
+            linearLayout = v.findViewById(R.id.recyclerviewLinear);
+        }
+
+        public void setData(HomeListDataModel dm){
+            this.textView.setText(dm.getText());
+            this.imgView.setImageResource(dm.getImage());
+        }
+    }
+
+    public HomeListAdapter(ArrayList<HomeListDataModel> values){
+        this.mDataList = values;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_home_list_layout,
+                parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        MyViewHolder vh = (MyViewHolder) holder;
+        vh.setData(mDataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+}
--- app/src/main/java/ca/uqac/alterra/home/HomeListFragment.java
+++ app/src/main/java/ca/uqac/alterra/home/HomeListFragment.java
@@ -9,8 +9,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 import ca.uqac.alterra.R;
 
@@ -18,9 +22,11 @@ public class HomeListFragment extends Fragment {
 
     FloatingActionButton mCameraButton;
 
+    private ArrayList<HomeListDataModel> mDataList;
+    private RecyclerView mRecyclerView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                               Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home_list,container,false);
     }
 
@@ -36,5 +42,20 @@ public class HomeListFragment extends Fragment {
 
         mCameraButton = getView().findViewById(R.id.cameraButton);
         mCameraButton.setOnClickListener((view) -> ((HomeActivity) getActivity()).dispatchTakePictureIntent());
+
+        mDataList = new ArrayList<>();
+
+        mRecyclerView = getView().findViewById(R.id.recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mDataList.add(new HomeListDataModel("Item 1",R.drawable.about));
+        mDataList.add(new HomeListDataModel("Item 2",R.drawable.about));
+        mDataList.add(new HomeListDataModel("Item 3",R.drawable.about));
+        mDataList.add(new HomeListDataModel("Item 4",R.drawable.about));
+
+        HomeListAdapter recyclerAdapter =  new HomeListAdapter(mDataList);
+        mRecyclerView.setAdapter(recyclerAdapter);
+
     }
 }
