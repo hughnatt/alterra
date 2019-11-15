054f2b1e4a5a8affe74dfaef27deb96db177f712
Hugh Natt <hughnatt@protonmail.com>
2019-11-15 16:26:04 -0500

    Transform hard-coded text to xml ressources

--- app/src/main/java/ca/uqac/alterra/auth/LoginFragment.java
+++ app/src/main/java/ca/uqac/alterra/auth/LoginFragment.java
@@ -1,5 +1,6 @@
 package ca.uqac.alterra.auth;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,7 +9,6 @@ import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +21,8 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Objects;
+
 import ca.uqac.alterra.R;
 import ca.uqac.alterra.database.AlterraAuth;
 import ca.uqac.alterra.database.AlterraCloud;
@@ -30,9 +32,6 @@ import ca.uqac.alterra.database.exceptions.AlterraAuthUserCollisionException;
 
 
 public class LoginFragment extends Fragment implements View.OnKeyListener {
-
-    private static final String TAG = LoginFragment.class.getSimpleName();
-
 
     private TextInputEditText emailEditText;
     private TextInputLayout emailTextInput;
@@ -79,48 +78,44 @@ public class LoginFragment extends Fragment implements View.OnKeyListener {
 
 
         Button googleButton = view.findViewById(R.id.googleButton);
-        googleButton.setOnClickListener((v) -> {
-            mAuth.logInWithGoogle(this, new AlterraAuth.AlterraAuthListener() {
-                @Override
-                public void onSuccess(AlterraUser user) {
-                    AlterraCloud.getDatabaseInstance().registerAlterraUser(user.getUID(),user.getEmail());
-                    mListener.onLoginSuccessful();
-                }
+        googleButton.setOnClickListener((v) -> mAuth.logInWithGoogle(this, new AlterraAuth.AlterraAuthListener() {
+            @Override
+            public void onSuccess(AlterraUser user) {
+                AlterraCloud.getDatabaseInstance().registerAlterraUser(user.getUID(),user.getEmail());
+                mListener.onLoginSuccessful();
+            }
 
-                @Override
-                public void onFailure(AlterraAuthException e) {
-                    if (e instanceof AlterraAuthUserCollisionException){
-                        Toast.makeText(getContext(), "Authentication failed. An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.",
-                                Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getContext(), "Authentication failed.",
-                                Toast.LENGTH_LONG).show();
-                    }
+            @Override
+            public void onFailure(AlterraAuthException e) {
+                if (e instanceof AlterraAuthUserCollisionException){
+                    Toast.makeText(getContext(), R.string.auth_exception_user_collision_long,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(),R.string.auth_login_failed,
+                            Toast.LENGTH_LONG).show();
                 }
-            });
-        });
+            }
+        }));
 
         Button facebookButton = view.findViewById(R.id.facebookButton);
-        facebookButton.setOnClickListener((v) -> {
-            mAuth.logInWithFacebook(this, new AlterraAuth.AlterraAuthListener() {
-                @Override
-                public void onSuccess(AlterraUser user) {
-                    AlterraCloud.getDatabaseInstance().registerAlterraUser(user.getUID(),user.getEmail());
-                    mListener.onLoginSuccessful();
-                }
+        facebookButton.setOnClickListener((v) -> mAuth.logInWithFacebook(this, new AlterraAuth.AlterraAuthListener() {
+            @Override
+            public void onSuccess(AlterraUser user) {
+                AlterraCloud.getDatabaseInstance().registerAlterraUser(user.getUID(),user.getEmail());
+                mListener.onLoginSuccessful();
+            }
 
-                @Override
-                public void onFailure(AlterraAuthException e) {
-                    if (e instanceof AlterraAuthUserCollisionException){
-                        Toast.makeText(getContext(), "Authentication failed. An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.",
-                                Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getContext(), "Authentication failed.",
-                                Toast.LENGTH_LONG).show();
-                    }
+            @Override
+            public void onFailure(AlterraAuthException e) {
+                if (e instanceof AlterraAuthUserCollisionException){
+                    Toast.makeText(getContext(), R.string.auth_exception_user_collision_long,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(), R.string.auth_login_failed,
+                            Toast.LENGTH_LONG).show();
                 }
-            });
-        });
+            }
+        }));
 
         setEmailTextListener();
         setPasswordTextListener();
@@ -145,28 +140,27 @@ public class LoginFragment extends Fragment implements View.OnKeyListener {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        Log.d("DEBUG","requestCode = " + requestCode);
         mAuth.getCallback().onActivityResult(requestCode,resultCode,data);
         super.onActivityResult(requestCode, resultCode, data);
 
     }
 
-    protected void setLoginListener(LoginListener listener) { mListener = listener; }
+    void setLoginListener(LoginListener listener) { mListener = listener; }
 
 
     private void verifyFields(){
-        String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
+        String email = Objects.requireNonNull(emailEditText.getText()).toString();
+        String password = Objects.requireNonNull(passwordEditText.getText()).toString();
 
         boolean isValid = true;
 
         if(email.length() <1){
-            emailTextInput.setError("Please enter email");
+            emailTextInput.setError(getString(R.string.auth_enter_email));
             isValid = false;
         }
 
         if(password.length() <1){
-            passwordTextInput.setError("Please enter password");
+            passwordTextInput.setError(getString(R.string.auth_enter_password));
             isValid = false;
         }
 
@@ -179,11 +173,14 @@ public class LoginFragment extends Fragment implements View.OnKeyListener {
 
                 @Override
                 public void onFailure(AlterraAuthException e) {
-                    new MaterialAlertDialogBuilder(getContext(), R.style.DialogStyle)
-                            .setTitle("Login Failed")
-                            .setMessage(e.getMessage())
-                            .setPositiveButton("OK", null)
-                            .show();
+                    Context context = getContext();
+                    if (context != null) {
+                        new MaterialAlertDialogBuilder(context, R.style.DialogStyle)
+                                .setTitle(R.string.auth_login_failed)
+                                .setMessage(e.getMessage())
+                                .setPositiveButton(R.string.auth_generic_positive_answer, null)
+                                .show();
+                    }
                 }
             });
 
