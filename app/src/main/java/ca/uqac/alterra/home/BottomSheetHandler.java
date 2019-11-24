58b232505f43926a8b8b504e392cbbf003f51fe0
Estelle LANDI <estelle.landi@etu.univ-grenoble-alpes.fr>
2019-11-24 17:23:25 -0500

    Display info from alterra point

--- app/src/main/java/ca/uqac/alterra/home/BottomSheetHandler.java
+++ app/src/main/java/ca/uqac/alterra/home/BottomSheetHandler.java
@@ -2,6 +2,7 @@ package ca.uqac.alterra.home;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,10 +21,21 @@ public class BottomSheetHandler extends BottomSheetBehavior.BottomSheetCallback 
 
     private FloatingActionButton mCameraButton;
     private TextView mTitle;
+    private TextView mDistance;
+    private TextView mAddress;
+    private TextView mDescription;
+
+    private LinearLayout bsDescriptionLinLayout;
+    private LinearLayout bsAddressLinLayout;
 
     public BottomSheetHandler(Activity activity){
         mCameraButton = activity.findViewById(R.id.cameraButton);
         mTitle = activity.findViewById(R.id.bottomPanelTitle);
+        mDistance =activity.findViewById(R.id.distance);
+        mAddress = activity.findViewById(R.id.locationAddress);
+        mDescription = activity.findViewById(R.id.locationDescription);
+        bsAddressLinLayout = activity.findViewById(R.id.BSAddressLinLayout);
+        bsDescriptionLinLayout = activity.findViewById(R.id.BSDescriptionLinLayout);
     }
 
     @Override
@@ -44,5 +56,12 @@ public class BottomSheetHandler extends BottomSheetBehavior.BottomSheetCallback 
 
     public void updateSheet(AlterraPoint alterraPoint){
         mTitle.setText(alterraPoint.getTitle());
+        if(alterraPoint.getDescription().isEmpty())
+            bsDescriptionLinLayout.setVisibility(View.GONE);
+        else{
+            mDescription.setText(alterraPoint.getDescription());
+            bsDescriptionLinLayout.setVisibility(View.VISIBLE);
+        }
+        mAddress.setText(alterraPoint.getLatLng().toString());
     }
 }
