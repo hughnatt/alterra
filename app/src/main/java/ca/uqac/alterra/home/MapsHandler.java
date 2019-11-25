7c0a262e91a4b1831dbb8c3faf69723de88f7e1d
Hugh Natt <hughnatt@protonmail.com>
2019-11-25 15:10:10 -0500

    Fix issue 62 missing import

--- app/src/main/java/ca/uqac/alterra/home/MapsHandler.java
+++ app/src/main/java/ca/uqac/alterra/home/MapsHandler.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Objects;
 
 import ca.uqac.alterra.R;
+import ca.uqac.alterra.utility.AlterraGeolocator;
 import ca.uqac.alterra.utility.JsonReader;
 
 public class MapsHandler implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnCameraMoveListener, GoogleMap.OnMapClickListener {
