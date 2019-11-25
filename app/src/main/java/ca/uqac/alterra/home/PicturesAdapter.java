f8ebc22e41f67ffa57e03ab7ca8e32544da8c6f8
qsibue <quentin.sibue@live.fr>
2019-11-25 22:22:50 +0100

    add dialog box for deleting picture

--- app/src/main/java/ca/uqac/alterra/home/PicturesAdapter.java
+++ app/src/main/java/ca/uqac/alterra/home/PicturesAdapter.java
@@ -1,6 +1,8 @@
 package ca.uqac.alterra.home;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 
 import java.util.ArrayList;
@@ -47,6 +50,30 @@ public class PicturesAdapter extends RecyclerView.Adapter{
            public void onClick(View v){
                 switchContext(mPhotoList.get(position));
            }
+       });
+
+       Pholder.mPlace.setOnLongClickListener(new View.OnLongClickListener() {
+           @Override
+           public boolean onLongClick(View view) {
+               new MaterialAlertDialogBuilder(mContext)
+                       .setTitle(R.string.profile_photos_dialog_box_title)
+                       .setMessage(R.string.profile_photos_dialog_box_message)
+
+                       // Specifying a listener allows you to take an action before dismissing the dialog.
+                       // The dialog is automatically dismissed when a dialog button is clicked.
+                       .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                           public void onClick(DialogInterface dialog, int which) {
+                               // Continue with delete operation
+                           }
+                       })
+
+                       // A null listener allows the button to dismiss the dialog and take no further action.
+                       .setNegativeButton(android.R.string.no, null)
+                       .setIcon(android.R.drawable.ic_dialog_alert)
+                       .show();
+               return true;
+           }
+
        });
     }
 
