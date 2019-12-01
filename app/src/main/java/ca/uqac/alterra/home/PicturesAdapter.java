e7242031678cff9f69fdc238aa25ec8e49e6fa2e
qsibue <quentin.sibue@live.fr>
2019-12-01 17:56:46 +0100

    refactor the adapter with interfaces

--- app/src/main/java/ca/uqac/alterra/home/PicturesAdapter.java
+++ app/src/main/java/ca/uqac/alterra/home/PicturesAdapter.java
@@ -1,18 +1,17 @@
 package ca.uqac.alterra.home;
 
-import android.app.AlertDialog;
+
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 
 import java.util.ArrayList;
@@ -23,10 +22,14 @@ public class PicturesAdapter extends RecyclerView.Adapter{
 
     private Context mContext;
     private ArrayList<String> mPhotoList;
+    private OnPictureClickListener mPictureClickListener;
+    private OnPictureLongClickListener mPictureLongClickListener;
 
-    public PicturesAdapter(Context mContext) {
+    public PicturesAdapter(Context mContext, @Nullable OnPictureClickListener pictureClickListener, @Nullable OnPictureLongClickListener pictureLongClickListener) {
         this.mContext = mContext;
         this.mPhotoList = new ArrayList<>();
+        this.mPictureClickListener = pictureClickListener;
+        this.mPictureLongClickListener = pictureLongClickListener;
     }
 
     @Override
@@ -45,43 +48,18 @@ public class PicturesAdapter extends RecyclerView.Adapter{
             .fitCenter()
             .into(((PlaceViewHolder) holder).mPlace);
 
-       Pholder.mPlace.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View v){
-                switchContext(mPhotoList.get(position));
-           }
-       });
+        Pholder.mPlace.setOnClickListener(view -> {
+            if(mPictureClickListener != null){
+                mPictureClickListener.onClick(mPhotoList.get(position));
+            }
+        });
+        Pholder.mPlace.setOnLongClickListener(view -> {
+            if(mPictureLongClickListener != null){
+                mPictureLongClickListener.onLongClick(mPhotoList.get(position),position);
+            }
+            return true;
+        });
 
-       Pholder.mPlace.setOnLongClickListener(new View.OnLongClickListener() {
-           @Override
-           public boolean onLongClick(View view) {
-               new MaterialAlertDialogBuilder(mContext)
-                       .setTitle(R.string.profile_photos_dialog_box_title)
-                       .setMessage(R.string.profile_photos_dialog_box_message)
-
-                       // Specifying a listener allows you to take an action before dismissing the dialog.
-                       // The dialog is automatically dismissed when a dialog button is clicked.
-                       .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                           public void onClick(DialogInterface dialog, int which) {
-                               // Continue with delete operation
-                           }
-                       })
-
-                       // A null listener allows the button to dismiss the dialog and take no further action.
-                       .setNegativeButton(android.R.string.no, null)
-                       .setIcon(android.R.drawable.ic_dialog_alert)
-                       .show();
-               return true;
-           }
-
-       });
-    }
-
-    public void switchContext(String url){
-        if(mContext instanceof HomeActivity){
-            HomeActivity homeActivity =(HomeActivity) mContext;
-            homeActivity.displayPicture(url);
-        }
     }
 
     @Override
@@ -102,5 +80,13 @@ public class PicturesAdapter extends RecyclerView.Adapter{
 
             mPlace = itemView.findViewById(R.id.ivPlace);
         }
+    }
+
+    public interface OnPictureClickListener{
+        void onClick(String url);
+    }
+
+    public interface OnPictureLongClickListener{
+        void onLongClick(String url,int position);
     }
 }
