3059d8fde628d1c973689edf30b603853a8cdbf3
Hugh Natt <hughnatt@protonmail.com>
2019-12-01 12:25:37 -0500

    Connect details fragment to main activity

--- app/src/main/java/ca/uqac/alterra/home/HomeDetailsFragment.java
+++ app/src/main/java/ca/uqac/alterra/home/HomeDetailsFragment.java
@@ -15,13 +15,38 @@ import ca.uqac.alterra.R;
 
 public class HomeDetailsFragment extends Fragment {
 
+    private static final int COLUMN_COUNT = 3;
+    private static final String ARGS_ALTERRA_POINT = "ARGS_ALTERRA_POINT";
+
+    private AlterraPoint mAlterraPoint;
+    private RecyclerView mPicturesRecyclerView;
+
+    public static HomeDetailsFragment newInstance(AlterraPoint alterraPoint) {
+
+        Bundle args = new Bundle();
+        args.putSerializable(ARGS_ALTERRA_POINT,alterraPoint);
+
+        HomeDetailsFragment fragment = new HomeDetailsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_home_details,container,false);
 
-        RecyclerView picturesRecyclerView = v.findViewById(R.id.detailsRecyclerView);
-        picturesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+        mPicturesRecyclerView = v.findViewById(R.id.detailsRecyclerView);
+        mPicturesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),COLUMN_COUNT));
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        PicturesAdapter picturesAdapter = new PicturesAdapter(getContext(),null,null);
+        mPicturesRecyclerView.setAdapter(picturesAdapter);
+
     }
 }
